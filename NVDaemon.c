ec6f2954875429669298384e85a1514fb02b4bed
YiSH <yishanhe0203@gmail.com>
2013-10-30 16:24:39 -0400

    new file:   NVDaemon.c 	new file:   nvm.daemon/Log.txt 	new file:   prepare/daemon_experiment.c 	new file:   prepare/mmap_experiment.c

---
+++ NVDaemon.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  NVDaemon.c
+ *
+ *    Description:  a daemon keeps running and can start subprocesses
+ *
+ *        Version:  1.0
+ *        Created:  10/25/2013 02:07:16 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  SYI (),
+ *   Organization:  CS@WM
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
