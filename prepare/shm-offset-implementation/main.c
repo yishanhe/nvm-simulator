eb48025a992b8093f69050d2d58bb4d345587abd
syi@alienware <syi@email.wm.edu>
2013-11-12 15:20:35 -0500

    update

---
+++ prepare/shm-offset-implementation/main.c
@@ -0,0 +1,59 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  11/02/2013 09:12:31 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "global.h"
+#include "nv_api.h"
+
+int main(int argc, const char *argv[])
+{
+    int shmid;
+    int pid;
+    NVRDescr * nvrAddr;
+    char name[]="/home/syi/GitRepo/nvm-simulator/nvm.daemon/NVRegion1";
+
+    // create shared memory
+
+    if((pid=fork())<0) {
+        perror("Fail to fork");
+        exit(EXIT_FAILURE);
+    }else if(pid==0) {
+        nvrAddr = NVOpenRegion(name,0,SHM_SIZE);
+        shmid = nvrAddr->ID;
+        printf("child shmid is %d\n",shmid);
+    }else{
+        wait(NULL);
+        nvrAddr = NVOpenRegion(name,0,SHM_SIZE);
+        shmid = nvrAddr->ID;
+        printf("parent shmid is %d\n",shmid);
+        NVRDescrDump(nvrAddr);
+        //char namee[]="/home/syi/GitRepo/nvm-simulator/nvm.daemon/NVRegion2";
+        if(NVDeleteRegion(name)==-1){
+            perror("Fail to shmctl");
+            exit(EXIT_FAILURE);
+        }
+    }
+    exit(EXIT_SUCCESS);
+}
